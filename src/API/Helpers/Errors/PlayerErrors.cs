cd46f852c27e2de85fd126e656906eeee1b73d02
Kristian Jeremic <kristianjer@gmail.com>
2024-03-18 12:05:27 +0100

    Adjust error text

--- src/API/Helpers/Errors/PlayerErrors.cs
+++ src/API/Helpers/Errors/PlayerErrors.cs
@@ -3,7 +3,7 @@ namespace API.Helpers.Errors;
 
 public record PlayerErrors()
 {
-    public static Error NotFound(int PlayerId) =>  new($"Players.NotFound", $"Tournament with id {PlayerId} not found.");
+    public static Error NotFound(int PlayerId) =>  new($"Players.NotFound", $"Player with id {PlayerId} not found.");
     public static Error NotRegisteredInParentTournament(int PlayerId, int ParentTournament) =>  new("Players.NotRegisteredInParentTournament", $"Player with id {PlayerId} is not registered in parent tournament with id {ParentTournament}");
 
 }
